8f6befa7fead10ce1c5550afadc53cb27c7ca36a
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-04-03 14:50:05 +0300

    Added page loader

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,18 +1,10 @@
+//Loader
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader-hidden");
 
-$(document).ready(function () {
-    // Get the offcanvas element
-    var offcanvas = $('#offcanvasExample');
-
-    // Get the dismiss button
-    var dismissBtn = offcanvas.find('.btn-close');
-
-    // Add click event listener to the dismiss button
-    dismissBtn.on('click', function () {
-        // Remove the "offcanvas" class from the body element
-        $('body').removeClass('offcanvas');
-    });
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
 });
-
-console.log('Script loaded successfully');
-
 
