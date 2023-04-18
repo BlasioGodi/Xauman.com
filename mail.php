9bbe6a9cc46154b30cc4fb5a26f8070f8beab068
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-04-18 12:48:45 +0300

    Added database connectivity for testing

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-include 'config.php';
+include_once 'config.php';
 
 //Phpmailer
 use PHPMailer\PHPMailer\PHPMailer;
@@ -9,12 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // Output messages
 $responses = [];
-
 $email = $_POST["email"];
-
-$emailSent = ''; //Email sent declaration
-
-$htmlcontent = file_get_contents('/email/xauman_email_2.html');
 
 // Check if the form was submitted
 if (isset($email)) {
@@ -22,7 +17,6 @@ if (isset($email)) {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $responses[] = 'Email is not valid!';
     }
-
     // Make sure the form fields are not empty
     if (empty($email)) {
         $responses[] = 'Please complete all fields!';
@@ -30,44 +24,53 @@ if (isset($email)) {
     // If there are no errors
     if (!$responses) {
 
-        //SMTP Configuration settings
-        $phpmailer = new PHPMailer(true);
-        $phpmailer->SMTPSecure = 'ssl';
-        $phpmailer->isSMTP();
-        $phpmailer->Host = 'smtp.titan.email';
-        $phpmailer->SMTPAuth = true;
-        $phpmailer->Port = 465;
-        $phpmailer->Username = 'info@xauman.com';
-        $phpmailer->Password = 'traderjosh@2023';
-        $phpmailer->setFrom('info@xauman.com', 'Xauman Forex Website');
-        $phpmailer->addReplyTo($email, 'Sender Details');
-        $phpmailer->addAddress('info@xauman.com', 'Xauman GMAIL');
-        $phpmailer->addBCC('muhindablasio@gmail.com', 'Xauman Titan');
-        $phpmailer->Subject = "Website Message";
-        $phpmailer->Sender = 'info@xauman.com';
-        $exception = new Exception();
+        if (isset($_POST['sub_button'])) {
 
-        //SMTP Debug setting
-        $phpmailer->SMTPDebug = 2;
+            $sql = "INSERT INTO subscribers_details (email_address)
+                VALUES ('$email')";
 
-        // Enable HTML if needed
-        $phpmailer->isHTML(true);
-        $bodyParagraphs = ["Email: {$email}"];
-        $body = join('<br />', $bodyParagraphs);
-        $phpmailer->Body = $body;
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
 
-        if ($phpmailer->send() == true) {
-            // Success
-            $responses[] = 'Message Sent!';
-            $emailSent = 'success';
-            echo ($emailSent);
-        } else {
-            // Error
-            $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $phpmailer->ErrorInfo;
-            $emailSent = 'error';
-            echo ($emailSent);
+            $conn->close();
+        }
+        try {
+            //Server Settings
+            $phpmailer = new PHPMailer(true);
+            $phpmailer->SMTPSecure = 'ssl';
+            $phpmailer->isSMTP();
+            $phpmailer->Host = 'smtp.titan.email';
+            $phpmailer->SMTPAuth = true;
+            $phpmailer->Port = 465;
+            $phpmailer->Username = 'info@xauman.com';
+            $phpmailer->Password = 'traderjosh@2023';
+            $phpmailer->setFrom('info@xauman.com', 'Xauman Forex');
+            $phpmailer->addReplyTo($email, 'Sender Details');
+            $phpmailer->addAddress($email, 'Commodities Trader');
+            $phpmailer->addBCC('muhindablasio@gmail.com', 'Administrator');
+
+            //SMTP Debug Setting
+            $phpmailer->SMTPDebug = 0;
+
+            //Email Sender & Subject
+            $phpmailer->Sender = 'info@xauman.com';
+            $phpmailer->Subject = "Welcome to Xauman Forex!";
+
+            //Read the contents of the html file
+            $htmlcontent = file_get_contents('email/xauman_email.html');
+
+            // Enable HTML if needed
+            $phpmailer->isHTML(true);
+            $phpmailer->Body = $htmlcontent;
+
+            $phpmailer->send();
+
+        } catch (Exception $exception) {
+            $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $exception->errorMessage();
         }
     }
 }
-
 ?>
